1454d814e4cd9af095567b59569f887b6f781fe0
Austin Williams <austinq.williams@gmail.com>
2019-02-24 22:01:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 && 5 == 0
+def fizzbuzz(number)
+  if number % 3 == 0 && 5 == 0
     return "Fizzbuzz"
 end 
 
-if int % 5 == 0
+if number % 5 == 0
    return "Buzz"
   end
   
-  if int % 3 == 0 
+  if number % 3 == 0 
     return "Fizzbuzz"
   end 
 end 
